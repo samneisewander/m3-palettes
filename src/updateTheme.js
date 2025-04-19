a511454ba9c19c1c2048053b27524b9bc79dc87a
Sam Neisewander <samuelneisewander@gmail.com>
2025-04-19 14:21:09 -0500

    Fix: Support class strategy

--- src/updateTheme.js
+++ src/updateTheme.js
@@ -1,77 +1,94 @@
-import { tailwindThemeFromColor } from "./tailwindThemeFromColor";
-import Color from "color";
+import { tailwindThemeFromColor } from './tailwindThemeFromColor'
+import Color from 'color'
 
-const DYNAMIC_THEME_STYLESHEET_ID = "tailwind-material-colors-dynamic-theme";
+const DYNAMIC_THEME_STYLESHEET_ID = 'tailwind-material-colors-dynamic-theme'
 
 export const updateTheme = (
-  colorsMap,
-  darkModeConfig = "media",
-  scheme = "content",
-  contrast = 0
+	colorsMap,
+	darkModeConfig = 'media',
+	scheme = 'content',
+	contrast = 0
 ) => {
-  let newCSS = "";
+	let newCSS = ''
 
-  const LIGHT_SELECTOR = ":root";
-  const DARK_SELECTOR = Array.isArray(darkModeConfig)
-    ? darkModeConfig[0] === "class"
-      ? darkModeConfig[1] || ".dark"
-      : "@media (prefers-color-scheme: dark)"
-    : darkModeConfig === "class"
-    ? ".dark"
-    : "@media (prefers-color-scheme: dark)";
+	const LIGHT_SELECTOR = ':root'
+	const DARK_SELECTOR = Array.isArray(darkModeConfig)
+		? darkModeConfig[0] === 'class'
+			? darkModeConfig[1] || '.dark'
+			: '@media (prefers-color-scheme: dark)'
+		: darkModeConfig === 'class'
+		? '.dark'
+		: '@media (prefers-color-scheme: dark)'
 
-  if (colorsMap.primary) {
-    const { black, white, transparent, current, ...colors } =
-      tailwindThemeFromColor(colorsMap, scheme, contrast);
+	if (colorsMap.primary) {
+		const { black, white, transparent, current, ...colors } =
+			tailwindThemeFromColor(colorsMap, scheme, contrast)
 
-    Object.keys(colors).forEach((lightColorName) => {
-      const match = lightColorName.match(
-        new RegExp(/^(?:(.+)-)?light(?:-(.+))?$/)
-      );
-      if (match) {
-        const prefix = match[1];
-        const suffix = match[2];
+		Object.keys(colors).forEach((lightColorName) => {
+			const match = lightColorName.match(
+				new RegExp(/^(?:(.+)-)?light(?:-(.+))?$/)
+			)
+			if (match) {
+				const prefix = match[1]
+				const suffix = match[2]
 
-        const modeAwareColorName = [prefix, suffix].filter((x) => x).join("-");
-        const darkColorName = [prefix, "dark", suffix]
-          .filter((x) => x)
-          .join("-");
+				const modeAwareColorName = [prefix, suffix]
+					.filter((x) => x)
+					.join('-')
+				const darkColorName = [prefix, 'dark', suffix]
+					.filter((x) => x)
+					.join('-')
 
-        const lightColor = colors[lightColorName];
-        const darkColor = colors[darkColorName];
+				const lightColor = colors[lightColorName]
+				const darkColor = colors[darkColorName]
 
-        if (lightColor && darkColor) {
-          newCSS += `
+				if (lightColor && darkColor) {
+					newCSS += `
           ${LIGHT_SELECTOR} {
             --color-${modeAwareColorName}: ${Color(lightColor)
-            .rgb()
-            .array()
-            .join(" ")};
+						.rgb()
+						.array()
+						.join(' ')};
           }
-          ${DARK_SELECTOR} {
-            ${LIGHT_SELECTOR} {
-              --color-${modeAwareColorName}: ${Color(darkColor)
-              .rgb()
-              .array()
-              .join(" ")};
-            }
-          }
-        `;
-        }
-      }
-    });
+          `
+					if (
+						DARK_SELECTOR === '@media (prefers-color-scheme: dark)'
+					) {
+						newCSS += `
+              ${DARK_SELECTOR} {
+                ${LIGHT_SELECTOR} {
+                  --color-${modeAwareColorName}: ${Color(darkColor)
+							.rgb()
+							.array()
+							.join(' ')};
+                }
+              }
+            `
+					} else {
+						newCSS += `
+              ${DARK_SELECTOR} {
+                --color-${modeAwareColorName}: ${Color(darkColor)
+							.rgb()
+							.array()
+							.join(' ')};
+              }
+            `
+					}
+				}
+			}
+		})
 
-    newCSS.replace(/\s*\n\s*/g, " ");
+		newCSS.replace(/\s*\n\s*/g, ' ')
 
-    let stylesheet = document.getElementById(DYNAMIC_THEME_STYLESHEET_ID);
-    if (!stylesheet) {
-      stylesheet = document.createElement("style");
-      stylesheet.id = DYNAMIC_THEME_STYLESHEET_ID;
-      document.head.appendChild(stylesheet);
-    }
+		let stylesheet = document.getElementById(DYNAMIC_THEME_STYLESHEET_ID)
+		if (!stylesheet) {
+			stylesheet = document.createElement('style')
+			stylesheet.id = DYNAMIC_THEME_STYLESHEET_ID
+			document.head.appendChild(stylesheet)
+		}
 
-    stylesheet.innerText = newCSS;
-  } else {
-    throw "A primary color must be specified";
-  }
-};
+		stylesheet.innerText = newCSS
+	} else {
+		throw 'A primary color must be specified'
+	}
+}
